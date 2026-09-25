cf29616a9ca7cb80a548be6303d27bc18d9264e3
EdsonRicard00 <edrpj00@gmail.com>
2026-09-25 09:25:41 -0300

    Tabuada Invertida

---
+++ ExercicioFor03invertido.java
@@ -0,0 +1,18 @@
+public class ExercicioFor03invertido {
+	public static void main(String[] args) {
+		int i, j;
+
+		for(j=9;j>=2;j--)
+
+		{
+
+			System.out.println("TABUADA DO"+j);
+
+			
+
+			for (i=1;i<=10;i++)
+
+				System.out.println(j+"X"+i+"="+j*i);
+}
+}
+}
